cc8ffe5a348af13d8beb74d8352416a726ee6b9a
SwampStomp <ashkoda@caeonline.com>
2021-10-18 23:16:39 +0300

    Update: namespace className ets..

--- AddressBook-Web-Tests/AddressBook-Web-Tests/GroupCreationTests.cs
+++ AddressBook-Web-Tests/AddressBook-Web-Tests/GroupCreationTests.cs
@@ -7,10 +7,10 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.Support.UI;
 
-namespace AddressBook_Web_Tests
+namespace WebAddressBookTests
 {
     [TestFixture]
-    public class UntitledTestCase
+    public class GroupCreationTests
     {
         private IWebDriver driver;
         private StringBuilder verificationErrors;
@@ -40,7 +40,7 @@ namespace AddressBook_Web_Tests
         }
 
         [Test]
-        public void TheUntitledTestCaseTest()
+        public void GroupCreationTest()
         {
             driver.Navigate().GoToUrl(baseURL);
             driver.FindElement(By.Name("user")).Clear();
